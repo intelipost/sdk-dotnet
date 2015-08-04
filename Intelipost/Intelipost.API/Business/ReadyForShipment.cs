273d65f4d2b6056cf8cd2aea1a485d464cc6407b
rogerioms <sm.rogerio@hotmail.com>
2015-08-04 16:59:23 -0300

    Added Bussines Ready For Shipment

---
+++ Intelipost/Intelipost.API/Business/ReadyForShipment.cs
@@ -0,0 +1,20 @@
+﻿using Intelipost.API.Model;
+
+namespace Intelipost.API.Business
+{
+    /// <summary>
+    /// Classe destinada a tratar todas as regras de negócios, aplicadas a API, da cotação.
+    /// </summary>
+    internal class ReadyForShipment
+    {
+        /// <summary>
+        /// Executa a requisição contra a InteliPost.
+        /// </summary>
+        /// <param name="request">Entidade Request devidamente preenchida para a cotação.</param>
+        /// <returns>Retorna uma cotação preenchida ou mensagem de erro.</returns>
+        internal Response<Model.ReadyForShipment> RequestReadyForShipment(Request<Model.ReadyForShipment> request)
+        {
+            return new Infrastructure.JsonRequest.Request<Model.ReadyForShipment>().Execute(Configure.PublicInstance.ApiKey, Configure.PublicInstance.ApiUrl, "/shipment_order/ready_for_shipment", "POST", request);
+        }
+    }
+}
