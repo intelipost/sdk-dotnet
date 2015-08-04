4a773129a8210cf371d9866848418c86d8389b51
rogerioms <sm.rogerio@hotmail.com>
2015-08-04 17:18:48 -0300

    Added ReadyForShipment
    Added API and unit test

---
+++ Intelipost/Intelipost.API/ReadyForShipment.cs
@@ -0,0 +1,20 @@
+﻿using Intelipost.API.Model;
+
+namespace Intelipost.API
+{
+    /// <summary>
+    /// Classe destinada a expor as requisições para a cotação.
+    /// </summary>
+    public class ReadyForShipment
+    {
+        /// <summary>
+        /// Execute a requisição de Cotação.
+        /// </summary>
+        /// <param name="request">Entidade Request devidamente preenchida para a cotação.</param>
+        /// <returns>Retorna uma cotação preenchida ou mensagem de erro.</returns>
+        public Response<Model.ReadyForShipment> RequestReadyForShipment(Request<Model.ReadyForShipment> request)
+        {
+            return new Business.ReadyForShipment().RequestReadyForShipment(request);
+        }
+    }
+}
