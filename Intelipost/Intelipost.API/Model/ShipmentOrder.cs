fc9a02d614227a847d2b0e98696a8b01e1865be2
rogerioms <sm.rogerio@hotmail.com>
2015-08-07 11:44:29 -0300

    Adding fields on ShipmentOrder.cs model
    Adding fields on ShipmentOrder.cs model

--- Intelipost/Intelipost.API/Model/ShipmentOrder.cs
+++ Intelipost/Intelipost.API/Model/ShipmentOrder.cs
@@ -13,6 +13,18 @@ namespace Intelipost.API.Model
         /// <summary>
         /// Largura da caixa ou envelope.
         /// </summary>
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Largura da caixa ou envelope.
+        /// </summary>
+        [JsonProperty("client_id")]
+        public int ClientId { get; set; }
+
+        /// <summary>
+        /// Largura da caixa ou envelope.
+        /// </summary>
         [JsonProperty("quote_id")]
         public int QuoteId { get; set; }
 
@@ -21,6 +33,18 @@ namespace Intelipost.API.Model
         /// </summary>
         [JsonProperty("delivery_method_id")]
         public int DeliveryMethodId { get; set; }
+
+        /// <summary>
+        /// Largura da caixa ou envelope.
+        /// </summary>
+        [JsonProperty("provider_shipping_costs")]
+        public double ProviderShippingCosts { get; set; }
+
+        /// <summary>
+        /// Largura da caixa ou envelope.
+        /// </summary>
+        [JsonProperty("customer_shipping_costs")]
+        public double CustomerShippingCosts { get; set; }
 
         /// <summary>
         /// Comprimento da caixa (não é necessária para volumes do tipo ENVELOPE).
@@ -37,14 +61,68 @@ namespace Intelipost.API.Model
         /// <summary>
         /// Comprimento da caixa (não é necessária para volumes do tipo ENVELOPE).
         /// </summary>
-        [JsonProperty("shipment_order_volume_array")]
-        public List<ShipmentOrderVolumeArray> ShipmentOrderVolumeArray { get; set; }
+        [JsonProperty("estimated_delivery_date_iso")]
+        public string EstimatedDeliveryDateIso { get; set; }
+
+        /// <summary>
+        /// Comprimento da caixa (não é necessária para volumes do tipo ENVELOPE).
+        /// </summary>
+        [JsonProperty("estimated_delivery_date_lp")]
+        public string EstimatedDeliveryDateLp { get; set; }
+
+        /// <summary>
+        /// Comprimento da caixa (não é necessária para volumes do tipo ENVELOPE).
+        /// </summary>
+        [JsonProperty("shipped_date")]
+        public string ShippedDate { get; set; }
+
+        /// <summary>
+        /// Comprimento da caixa (não é necessária para volumes do tipo ENVELOPE).
+        /// </summary>
+        [JsonProperty("created")]
+        public string Created { get; set; }
+
+        /// <summary>
+        /// Comprimento da caixa (não é necessária para volumes do tipo ENVELOPE).
+        /// </summary>
+        [JsonProperty("created_iso")]
+        public string CreatedIso { get; set; }
+
+        /// <summary>
+        /// Comprimento da caixa (não é necessária para volumes do tipo ENVELOPE).
+        /// </summary>
+        [JsonProperty("modified")]
+        public string Modified { get; set; }
+
+        /// <summary>
+        /// Comprimento da caixa (não é necessária para volumes do tipo ENVELOPE).
+        /// </summary>
+        [JsonProperty("modified_iso")]
+        public string ModifiedIso { get; set; }
 
         /// <summary>
         /// Comprimento da caixa (não é necessária para volumes do tipo ENVELOPE).
         /// </summary>
         [JsonProperty("end_customer")]
         public EndCustomer EndCustomer { get; set; }
+
+        /// <summary>
+        /// Comprimento da caixa (não é necessária para volumes do tipo ENVELOPE).
+        /// </summary>
+        [JsonProperty("shipment_order_volume_state")]
+        public string ShipmentOrderVolumeState { get; set; }
+
+        /// <summary>
+        /// Comprimento da caixa (não é necessária para volumes do tipo ENVELOPE).
+        /// </summary>
+        [JsonProperty("delivered_date")]
+        public string DeliveredDate { get; set; }
+
+        /// <summary>
+        /// Comprimento da caixa (não é necessária para volumes do tipo ENVELOPE).
+        /// </summary>
+        [JsonProperty("shipment_order_volume_array")]
+        public List<ShipmentOrderVolumeArray> ShipmentOrderVolumeArray { get; set; }
 
         public ShipmentOrder()
         {
