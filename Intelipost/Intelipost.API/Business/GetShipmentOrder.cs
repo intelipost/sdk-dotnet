6045a6f08c3e5ff415c23a2a049cf4d6de0b2625
rogerioms <sm.rogerio@hotmail.com>
2015-08-13 15:59:23 -0300

    New - Get ShipmentOrder
    Added GET Shippment Order

---
+++ Intelipost/Intelipost.API/Business/GetShipmentOrder.cs
@@ -0,0 +1,20 @@
+﻿using Intelipost.API.Model;
+
+namespace Intelipost.API.Business
+{
+    /// <summary>
+    /// Classe destinada a tratar todas as regras de negócios, aplicadas a API, da cotação.
+    /// </summary>
+    internal class GetShipmentOrder
+    {
+        /// <summary>
+        /// Executa a requisição contra a InteliPost.
+        /// </summary>
+        /// <param name="request">Entidade Request devidamente preenchida para a cotação.</param>
+        /// <returns>Retorna uma cotação preenchida ou mensagem de erro.</returns>
+        internal Response<Model.GetShipmentOrder> RequestGetShipmentOrder(string OrderNumber)
+        {
+            return new Infrastructure.JsonRequest.Request<Model.GetShipmentOrder>().Execute(Configure.PublicInstance.ApiKey, Configure.PublicInstance.ApiUrl, "/shipment_order/"+ OrderNumber, "GET", null);
+        }
+    }
+}
---
+++ Intelipost/Intelipost.API/GetShipmentOder.cs
@@ -0,0 +1,20 @@
+﻿using Intelipost.API.Model;
+
+namespace Intelipost.API
+{
+    /// <summary>
+    /// Classe destinada a expor as requisições para a cotação.
+    /// </summary>
+    public class GetShipmentOrder
+    {
+        /// <summary>
+        /// Execute a requisição de Cotação.
+        /// </summary>
+        /// <param name="request">Entidade Request devidamente preenchida para a cotação.</param>
+        /// <returns>Retorna uma cotação preenchida ou mensagem de erro.</returns>
+        public Response<Model.GetShipmentOrder> RequestGetShipmentOrder(string OrderNumber)
+        {
+            return new Business.GetShipmentOrder().RequestGetShipmentOrder(OrderNumber);
+        }
+    }
+}
