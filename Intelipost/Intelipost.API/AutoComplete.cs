5ed54477a5ac30dc8bfd74fa87eb34137ab5aade
rogerioms <sm.rogerio@hotmail.com>
2015-08-14 18:38:09 -0300

    New AutoComplete

---
+++ Intelipost/Intelipost.API/AutoComplete.cs
@@ -0,0 +1,20 @@
+﻿using Intelipost.API.Model;
+
+namespace Intelipost.API
+{
+    /// <summary>
+    /// Classe destinada a expor as requisições para a cotação.
+    /// </summary>
+    public class AutoComplete
+    {
+        /// <summary>
+        /// Execute a requisição de Cotação.
+        /// </summary>
+        /// <param name="request">Entidade Request devidamente preenchida para a cotação.</param>
+        /// <returns>Retorna uma cotação preenchida ou mensagem de erro.</returns>
+        public Response<Model.AutoComplete> RequestAutoComplete(string cep)
+        {
+            return new Business.Autocomplete().RequestAutocomplete(cep);
+        }
+    }
+}
---
+++ Intelipost/Intelipost.UnitTest/TestAutocomplete.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Configuration;
+using Intelipost.API.Model;
+using Intelipost.API.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Intelipost.UnitTest
+{
+    /// <summary>
+    /// TestQuote é destinado a fazer uma cotação teste e verificar se o seu retorno está correto.
+    /// </summary>
+    [TestClass]
+    public class TestAutocomplete
+    {       
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        /// <summary>
+        /// TestRequest é destinado a requisição teste para a cotação.
+        /// </summary>
+        [TestMethod]
+        public void TestRequest()
+        {
+            new TestConfigure().TestInitialize();
+            try
+            {
+                var modelResponse = new API.AutoComplete().RequestAutoComplete("09932220");      
+                Assert.IsFalse(modelResponse.Status == "ERROR", "Houve algum problema na requisição, por favor, verifique o Log gerado para esta resposta da requisição.");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
