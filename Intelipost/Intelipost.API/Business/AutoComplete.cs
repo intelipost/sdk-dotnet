df562ece105ab8d814b7a5fbd6e6b43ab51cd545
rogerioms <sm.rogerio@hotmail.com>
2015-08-18 16:31:34 -0300

    updated desciption

--- Intelipost/Intelipost.API/Business/AutoComplete.cs
+++ Intelipost/Intelipost.API/Business/AutoComplete.cs
@@ -3,15 +3,14 @@
 namespace Intelipost.API.Business
 {
     /// <summary>
-    /// Classe destinada a tratar todas as regras de negócios, aplicadas a API, da cotação.
+    /// Classe destinada a tratar todas as regras de negócios, aplicadas a API.
     /// </summary>
     internal class Autocomplete
     {
         /// <summary>
         /// Executa a requisição contra a InteliPost.
         /// </summary>
-        /// <param name="request">Entidade Request devidamente preenchida para a cotação.</param>
-        /// <returns>Retorna uma cotação preenchida ou mensagem de erro.</returns>
+        /// <returns>Retorna as informações do CEP informado.</returns>
         internal Response<Model.AutoComplete> RequestAutocomplete(string cep)
         {
             return new Infrastructure.JsonRequest.Request<Model.AutoComplete>().Execute(Configure.PublicInstance.ApiKey, Configure.PublicInstance.ApiUrl, "/cep_location/address_complete/"+cep, "GET", null);
